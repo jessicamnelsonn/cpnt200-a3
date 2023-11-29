0a5c33be9e7b43e3fa7441f17219be0a815b7d05
Jessica Nelson <jessica.nelson60@gmail.com>
2023-11-28 17:45:30 -0700

    added styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,17 +2,20 @@
   export let data;
 </script>
 
-<ul>
+<h1 class="text-5xl text-center p-4"> Jessica's Journeys </h1>
   {#each data.blog_page as blog}
-      <article>
-        <h1>{blog.title}</h1>
-        <h2>{blog.author}</h2>
-        <h3>{blog.tags}</h3>
-        <p>{blog.date}</p>
-        <h2>{@html blog.rich_text}</h2>
-        <p>{blog.summary}</p>
+      <article class="p-4 m-6 border border-gray-300 rounded-md">
+        <h1 class="text-4xl font-bold mb-2">{blog.title}</h1>
+        <h2 class="text-lg text-gray-600 mb-2">Author: {blog.author}</h2>
+        <p class="text-gray-500 mb-2">{blog.date}</p>
+        <div class="mb-2">
+          <span class="text-blue-500">Tags: {blog.tags}</span>
+        </div>
+        <h2 class="text-3xl mb-4">{@html blog.rich_text}</h2>
+        <p class="text-gray-700">{blog.summary}</p>
       </article>
   {/each}
-</ul>
+
+
 
 
